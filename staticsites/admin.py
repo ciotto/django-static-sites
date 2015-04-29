2b710d1ce51e7e390a2af8154abad87f25d32bda
Christian Bianciotto <ciotto@users.noreply.github.com>
2015-04-29 23:10:44 +0200

    add admin.py

---
+++ staticsites/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from models import Deploy
+from models import DeployOperation
+
+
+class DeployAdmin(admin.ModelAdmin):
+    # Select visible fields on preview
+    list_display = ['id', 'type', 'date']
+
+
+# Customize DeployOperation admin page.
+class DeployOperationAdmin(admin.ModelAdmin):
+    # Select visible fields on preview
+    list_display = ['id', 'deploy', 'file_type', 'operation_type', 'path', 'md5', 'file_stogare']
+
+admin.site.register(Deploy, DeployAdmin)
+admin.site.register(DeployOperation, DeployOperationAdmin)
