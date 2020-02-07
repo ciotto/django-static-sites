eba6226c9692d26c23d20c3e8be238fe20d8ee43
ciotto <bianciotto@elipse.it>
2020-02-07 07:05:51 +0100

    fix pip session

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ __author__ = 'Christian Bianciotto'
 
 from setuptools import setup, find_packages
 try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
 except ImportError:
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 
 README = open('README.md').read()
@@ -17,7 +24,7 @@ install_requires = [str(ir.req) for ir in install_requires]
 
 setup(
     name='django-static-sites',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     package_data={'staticsites': ['templates/*/*-tpl', 'templates/*/*/*-tpl']},
     include_package_data=True,
@@ -27,9 +34,10 @@ setup(
         'console_scripts': ['django-static-admin=staticsites.management:execute_from_command_line'],
     },
     long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/ciotto/django-static-sites',
     author='Christian Bianciotto',
-    author_email='bianciotto@bitcircle.com',
+    author_email='info@ci8.it',
     package_dir={'django-static-sites': 'lib/django-static-sites'},
     classifiers=[
         'Environment :: Web Environment',
